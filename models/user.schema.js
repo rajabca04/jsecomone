0610bfdbe839b4582a65f3bbedd9840e40c6a6de
Raja kumar <72243994+rajabca04@users.noreply.github.com>
2022-11-27 06:59:23 +0000

    hooks for user schema

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import crypto from "crypto";
 
 // Creted a user schema with mongoose db
 const userSchema = mongoose.Schema(
@@ -35,6 +38,12 @@ const userSchema = mongoose.Schema(
 );
 
 
+// challenge 1 - encrypt password - hooks
+userSchema.pre("save", async function(next){
+    if(!this.modified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+})
 
 
 export default mongoose.model("User",userSchema);
