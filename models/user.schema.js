c90ec976cc605ea5b7884b68cfb3c985044cfa2e
Raja kumar <72243994+rajabca04@users.noreply.github.com>
2022-11-27 07:20:03 +0000

    jwt token

--- models/user.schema.js
+++ models/user.schema.js
@@ -3,47 +3,67 @@ import AuthRoles from "../utils/authRoles";
 import bcrypt from "bcryptjs";
 import JWT from "jsonwebtoken";
 import crypto from "crypto";
+import { config } from "process";
 
 // Creted a user schema with mongoose db
-const userSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "Name is required"],
-            maxLength: [50, "Name must be less the 50"]
-        },
-        email:{
-            type: String,
-            required: [true, "Email is required"],
-            unique: true
-        },
-        password:{
-            type: String,
-            required: [true, "Password is required"],
-            minLength: [8, "password must be less then 8  characters"],
-            select: false
-        },
-        roel:{
-            type: String,
-            enum: Object.values(AuthRoles),
-            default:AuthRoles.USER
-        },
-        forgotPasswordToken: String,
-        forgotPasswordExpiry: Date,
-
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        maxLength: [50, "Name must be less the 50"]
     },
-    {
-        timestamps: true
-    }
-);
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minLength: [8, "password must be less then 8  characters"],
+        select: false
+    },
+    roel: {
+        type: String,
+        enum: Object.values(AuthRoles),
+        default: AuthRoles.USER
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+
+}, {
+    timestamps: true
+});
 
 
 // challenge 1 - encrypt password - hooks
-userSchema.pre("save", async function(next){
-    if(!this.modified("password")) return next();
-    this.password = await bcrypt.hash(this.password,10)
+userSchema.pre("save", async function (next) {
+    if (!this.modified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
 
-export default mongoose.model("User",userSchema);
+// add more featuers directly to your schema
+userSchema.methods = {
+    //compare password
+    comparePassword: async function (enteredPassword) {
+        return await bcrypt.compare(enteredPassword, this.password)
+    },
+
+    // generate JWT token
+    getJwtToken: function () {
+        return JWT.sign(
+            {
+                _id: this._id,
+                role: this.role
+            },
+            config.JWT_SECRET,
+            {
+                expiresIn: config.JWT_EXPIRY
+            }
+        )
+    }
+}
+
+export default mongoose.model("User", userSchema);
